b3771996bac9bdc7745ab0dc644b9414e854309c
Viet Long Ta <long.ta1@ucalgary.ca>
2020-10-28 13:35:00 -0600

    Update DBManager

--- Reminder/src/Model/DBManager.java
+++ Reminder/src/Model/DBManager.java
@@ -1,7 +1,8 @@
 package Model;
-import javax.swing.*;
 import java.sql.*;
 
+
+//This class is responsible for creating tables if tables haven't been created.
 public class DBManager {
 
 	private static Connection conn = null;
@@ -11,7 +12,7 @@ public class DBManager {
 			Connection conn = DriverManager.getConnection("jdbc:sqlite:DBSM.sqlite");
 			return conn;
 		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, e);
+			e.printStackTrace();
 			return null;
 		}
 	}
@@ -19,27 +20,34 @@ public class DBManager {
 	public void getConnection() throws SQLException, ClassNotFoundException {
 		Class.forName("org.sqlite.JDBC");
 		conn = dbConnector();
-			initialise();
+		initialise();
 	}
 
 	private void initialise() throws SQLException {
+		dataBaseSetting();
 		createAlarmTable();
-		System.out.println("Create");
 		createMedicineTable();
 		createUserTable();
 	}
 	
+	private void dataBaseSetting() throws SQLException {
+		Statement state = conn.createStatement();
+		state.execute("PRAGMA foreign_keys = 1");
+	}
 	
+	//If table hasn't existed, create it
 	private void createMedicineTable() throws SQLException {
 		Statement state = conn.createStatement();
-		state.execute("CREATE TABLE if not exists Medicine(med_id integer,med_name varchar(60), primary key(med_id));");
+		state.execute("CREATE TABLE if not exists Medicine(med_id integer,med_name varchar(60),val INTEGER, unit varchar(5), primary key(med_id));");
 	}
 	
+	//If table hasn't existed, create it
 	private void createAlarmTable() throws SQLException {
 		Statement state = conn.createStatement();
-		state.execute("CREATE TABLE if not exists Alarm(id integer,fname varchar(60), primary key(id));");
+		state.execute("CREATE TABLE if not exists Alarm(id integer,med_id integer, alarm_name varchar(60), primary key(id),FOREIGN KEY(med_id) REFERENCES Medicine(med_id));");
 	}
 	
+	//If table hasn't existed, create it
 	private void createUserTable() throws SQLException {
 		Statement state = conn.createStatement();
 		state.execute("CREATE TABLE if not exists user(id integer,fname varchar(60),lName varchar(60), primary key(id));");
