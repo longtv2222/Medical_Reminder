134b43ffa7e37313b9375fcc475fc796e474b053
Viet Long Ta <long.ta1@ucalgary.ca>
2020-10-30 16:15:10 -0600

    Adding JAVAFX For Audio Processing and GUI

---
+++ Reminder/src/Model/Sound.java
@@ -0,0 +1,15 @@
+package Model;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Sound extends Application {
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		System.out.println("JAVAFX");
+	}
+}
