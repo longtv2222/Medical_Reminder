fd6f9287ff3674702988a4c64d0c1de5769a4f3f
Viet Long Ta <long.ta1@ucalgary.ca>
2020-10-30 17:16:47 -0600

    Successfully added Sound for JavaFx application

--- Reminder/src/Model/Sound.java
+++ Reminder/src/Model/Sound.java
@@ -1,15 +1,27 @@
 package Model;
 
+import java.io.File;
+
 import javafx.application.Application;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
-public class Sound extends Application {
-	public static void main(String[] args) {
-		launch(args);
-	}
+public class Sound extends Application  {
+	 public static void main(String[] args) {
+	        launch(args);
+	    }
+	    
+	    public void start(Stage primaryStage) {
+	    	File a = new File("Ringtone\\Default.mp3");
+	    	if(a.exists()) {
+		    	Media b = new Media(a.toURI().toString());
+		    	MediaPlayer c = new MediaPlayer(b);
+		    	c.play();
+	    	} else {
+	    		System.out.println("Cannot find the file you are looking for");
+	    	}
 
-	@Override
-	public void start(Stage arg0) throws Exception {
-		System.out.println("JAVAFX");
-	}
+	    }
 }
+	
