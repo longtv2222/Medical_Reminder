91a5c51dd14b1a9d02e40dd2205d7cd767d49726
Viet Long Ta <long.ta1@ucalgary.ca>
2020-10-31 11:28:37 -0600

    Add concurrency to project

--- Reminder/src/Model/Main.java
+++ Reminder/src/Model/Main.java
@@ -23,57 +23,57 @@ public class Main {
 			e.printStackTrace();
 		}
 
-//		  User user = new User("Long",2); 
-//		  ExecutorService executor = Executors.newSingleThreadExecutor();  //Create a thread for user to recursively check alarm ring
-//		  executor.execute(user);
-//
-//		int option = 0;
-//		Scanner scanner = new Scanner(System.in);
-//		boolean stop = false;
-//
-//		while (!stop) {
-//			System.out.println("\nPlease pick your option!");
-//			option = scanner.nextInt();
-//			scanner.nextLine();
-//			String medName;
-//			switch (option) {
-//			case 1:
-//				user.showAllMedicine();
-//				break;
-//			case 2:
-//				System.out.println("Please enter the name of the med");
-//				medName = scanner.nextLine();
-//				user.addMedicine(medName);
-//				break;
-//			case 3:
-//				System.out.println("Please enter the name of the med");
-//				medName = scanner.nextLine();
-//
-//				System.out.println("Please enter the hour");
-//				int hour = scanner.nextInt();
-//				scanner.nextLine();
-//
-//				System.out.println("Please enter the minute");
-//				int minute = scanner.nextInt();
-//				scanner.nextLine();
-//				user.addingAlarm(medName, new Alarm(hour, minute, 400, "ml"));
-//				break;
-//			case 4:
-//				System.out.println("Please enter the name of the med");
-//				medName = scanner.nextLine();
-//				user.printAllAlarm(medName);
-//				break;
-//			case 5:
-//				System.out.println("Bye");
-//				stop = true;
-//				user.recursiveCheckAlarm();
-//				break;
-//			default:
-//				System.out.println("Instruction not avaialable");
-//
-//			}
-//		}
-//		scanner.close();
+		  User user = new User("Long",2); 
+		  ExecutorService executor = Executors.newSingleThreadExecutor();  //Create a thread for user to recursively check alarm ring
+		  executor.execute(user);
+
+		int option = 0;
+		Scanner scanner = new Scanner(System.in);
+		boolean stop = false;
+
+		while (!stop) {
+			System.out.println("\nPlease pick your option!");
+			option = scanner.nextInt();
+			scanner.nextLine();
+			String medName;
+			switch (option) {
+			case 1:
+				user.showAllMedicine();
+				break;
+			case 2:
+				System.out.println("Please enter the name of the med");
+				medName = scanner.nextLine();
+				user.addMedicine(medName);
+				break;
+			case 3:
+				System.out.println("Please enter the name of the med");
+				medName = scanner.nextLine();
+
+				System.out.println("Please enter the hour");
+				int hour = scanner.nextInt();
+				scanner.nextLine();
+
+				System.out.println("Please enter the minute");
+				int minute = scanner.nextInt();
+				scanner.nextLine();
+				user.addingAlarm(medName, new Alarm(hour, minute, 400, "ml"));
+				break;
+			case 4:
+				System.out.println("Please enter the name of the med");
+				medName = scanner.nextLine();
+				user.printAllAlarm(medName);
+				break;
+			case 5:
+				System.out.println("Bye");
+				stop = true;
+				user.recursiveCheckAlarm();
+				break;
+			default:
+				System.out.println("Instruction not avaialable");
+
+			}
+		}
+		scanner.close();
 
 	}
 }
