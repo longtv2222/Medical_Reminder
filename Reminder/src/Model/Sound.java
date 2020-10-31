87249dc3385699761a1479a72bef84adcf740c8d
Viet Long Ta <long.ta1@ucalgary.ca>
2020-10-30 18:07:39 -0600

    Update code

--- Reminder/src/Model/Sound.java
+++ Reminder/src/Model/Sound.java
@@ -3,21 +3,25 @@ package Model;
 import java.io.File;
 
 import javafx.application.Application;
+import javafx.scene.media.AudioClip;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Sound extends Application  {
 	 public static void main(String[] args) {
 	        launch(args);
 	    }
-	    
+	   
 	    public void start(Stage primaryStage) {
 	    	File a = new File("Ringtone\\Default.mp3");
 	    	if(a.exists()) {
 		    	Media b = new Media(a.toURI().toString());
-		    	MediaPlayer c = new MediaPlayer(b);
+		    	AudioClip c = new AudioClip(b.getSource());
+		    	c.setCycleCount(0);
 		    	c.play();
+
 	    	} else {
 	    		System.out.println("Cannot find the file you are looking for");
 	    	}
