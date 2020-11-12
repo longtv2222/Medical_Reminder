f94b471eb855823af8cd54eeefaba02bdddf8bb4
longtv2222 <long.tv2222@gmail.com>
2020-11-11 21:33:47 -0700

    Basic Layout of Center Panel in GUI

--- Reminder/src/View/GUI.java
+++ Reminder/src/View/GUI.java
@@ -10,14 +10,17 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollBar;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -29,7 +32,7 @@ public class GUI extends Application {
 		launch(args);
 	}
 
-	Button oneSideRounded(String buttonName, int width, int height) {
+	private Button oneSideRounded(String buttonName, int width, int height) {
 		Button button = new Button(buttonName);
 		button.setPrefHeight(height);
 		button.setPrefWidth(width);
@@ -42,22 +45,68 @@ public class GUI extends Application {
 		return button;
 	}
 
-	VBox leftPanel() {
+	private VBox leftPanel() {
 		VBox vbox = new VBox();
-		
+
 		vbox.setId("left_panel");
 		VBox subVbox = new VBox();
 		subVbox.setId("subVbox");
-		
+
 		subVbox.getChildren().addAll(oneSideRounded("View alarm", 300, 60), oneSideRounded("View alarm", 300, 60),
-				oneSideRounded("View alarm", 300, 60), oneSideRounded("View alarm", 300, 60),oneSideRounded("View alarm", 300, 60),oneSideRounded("View alarm", 300, 60),oneSideRounded("View alarm", 300, 60),oneSideRounded("View alarm", 300, 60),oneSideRounded("View alarm", 300, 60));
+				oneSideRounded("View alarm", 300, 60), oneSideRounded("View alarm", 300, 60),
+				oneSideRounded("View alarm", 300, 60), oneSideRounded("View alarm", 300, 60),
+				oneSideRounded("View alarm", 300, 60), oneSideRounded("View alarm", 300, 60),
+				oneSideRounded("View alarm", 300, 60));
 		ScrollPane sp = new ScrollPane();
 		sp.setId("scroll_bar_left_panel");
 		sp.setContent(subVbox);
-		
+
 		Text welcome = new Text("Hi,Viet Long!\n");
 		welcome.setId("welcome_text");
-		vbox.getChildren().addAll(welcome,sp,subVbox);
+		vbox.getChildren().addAll(welcome, sp, subVbox);
+		return vbox;
+	}
+
+	private StackPane alarmNoti() {
+		StackPane sp = new StackPane();
+		GridPane grid = new GridPane();
+		grid.setHgap(10);
+		grid.setVgap(10);
+		Text alarm = new Text("WELCOME TO THE CITY OF DANVILLE");
+		Text noti = new Text("You need to take 30 mg of Pills");
+		grid.add(alarm, 4, 2);
+		grid.add(noti, 4, 3);
+		ToggleButton button = new ToggleButton("On");
+		grid.add(button, 10, 2);
+		Rectangle rect = new Rectangle(350, 100);
+		rect.setId("center_rectangle");
+		sp.getChildren().addAll(rect, grid);
+		return sp;
+	}
+
+	private VBox centerPanel() {
+		VBox vbox = new VBox();
+		vbox.setId("center_panel");
+
+		VBox subvbox = new VBox(30);
+
+		subvbox.setId("subVbox");
+		subvbox.getChildren().addAll(alarmNoti(), alarmNoti(), alarmNoti(), alarmNoti(), alarmNoti(), alarmNoti());
+
+		ScrollPane sp = new ScrollPane();
+		sp.setId("scroll_bar_center_panel");
+		sp.setContent(subvbox);
+
+		Text text = new Text("What to do today?\n");
+		text.setId("center_text");
+		vbox.getChildren().addAll(text, subvbox, sp);
+
+		return vbox;
+	}
+
+	private VBox rightPanel() {
+		VBox vbox = new VBox();
+		vbox.setId("right_panel");
 		return vbox;
 	}
 
@@ -66,10 +115,9 @@ public class GUI extends Application {
 		BorderPane borderPane = new BorderPane();
 		borderPane.setId("border_pane");
 
-
-
 		borderPane.setLeft(leftPanel());
-
+		borderPane.setCenter(centerPanel());
+		borderPane.setRight(rightPanel());
 
 		Scene homeScene = new Scene(borderPane);
 		homeScene.getStylesheets().add(css);
