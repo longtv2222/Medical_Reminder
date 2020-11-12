45ef02141fa293aac9448fc4e76472761c8dc8ae
longtv2222 <long.tv2222@gmail.com>
2020-11-11 18:05:06 -0700

    Finished left panel of GUI

--- Reminder/src/View/GUI.java
+++ Reminder/src/View/GUI.java
@@ -34,29 +34,46 @@ public class GUI extends Application {
 		button.setPrefHeight(height);
 		button.setPrefWidth(width);
 		button.setId("one_rounded_button");
-		
-		button.setOnMouseEntered(e->button.setId("hover_button"));
-		button.setOnMouseExited(e->button.setId("one_rounded_button"));
-		button.setOnMousePressed(e->button.setId("pressed_button"));
-		button.setOnMouseReleased(e->button.setId("hover_button"));
+
+		button.setOnMouseEntered(e -> button.setId("hover_button"));
+		button.setOnMouseExited(e -> button.setId("one_rounded_button"));
+		button.setOnMousePressed(e -> button.setId("pressed_button"));
+		button.setOnMouseReleased(e -> button.setId("hover_button"));
 		return button;
+	}
+
+	VBox leftPanel() {
+		VBox vbox = new VBox();
+		
+		vbox.setId("left_panel");
+		VBox subVbox = new VBox();
+		subVbox.setId("subVbox");
+		
+		subVbox.getChildren().addAll(oneSideRounded("View alarm", 300, 60), oneSideRounded("View alarm", 300, 60),
+				oneSideRounded("View alarm", 300, 60), oneSideRounded("View alarm", 300, 60),oneSideRounded("View alarm", 300, 60),oneSideRounded("View alarm", 300, 60),oneSideRounded("View alarm", 300, 60),oneSideRounded("View alarm", 300, 60),oneSideRounded("View alarm", 300, 60));
+		ScrollPane sp = new ScrollPane();
+		sp.setId("scroll_bar_left_panel");
+		sp.setContent(subVbox);
+		
+		Text welcome = new Text("Hi,Viet Long!\n");
+		welcome.setId("welcome_text");
+		vbox.getChildren().addAll(welcome,sp,subVbox);
+		return vbox;
 	}
 
 	@Override
 	public void start(Stage arg0) throws Exception {
-		Button a = oneSideRounded("View alarm", 200, 80);
 		BorderPane borderPane = new BorderPane();
 		borderPane.setId("border_pane");
-		
-		
-		VBox vbox = new VBox(30);
-		vbox.getChildren().addAll(a,oneSideRounded("View alarm", 200, 80),oneSideRounded("View alarm", 200, 80));
-		vbox.setId("left_panel");
-		borderPane.setLeft(vbox);
+
+
+
+		borderPane.setLeft(leftPanel());
+
 
 		Scene homeScene = new Scene(borderPane);
 		homeScene.getStylesheets().add(css);
-		
+
 		Stage home = new Stage();
 		home.setWidth(1280);
 		home.setHeight(720);
